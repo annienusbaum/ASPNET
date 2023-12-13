813bb13e56262431929f6de205d0b70d07261563
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-12-13 15:17:05 -0500

    ASPNET

---
+++ Testing/Models/ProductRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace Testing.Models
+{
+	public class ProductRepository : IProductRepository
+	{
+        private readonly IDbConnection _conn;
+
+		public ProductRepository(IDbConnection conn)
+		{
+			_conn = conn;
+		}
+
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+            return product;
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;", new { id = product.ProductID });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+			return _conn.Query<Product>("SELECT * From products");
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM CATEGORIES");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                new { id = id });
+        }
+
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO PRODUCTS (NAME, PRICE, CATEGORYID) VALUES(@name, @price, @categoryid);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryid = productToInsert.CategoryID });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE PRODUCTS SET NAME = @name, PRICE = @price WHERE PRODUCTID = @id",
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+    }
+	}
+
+//system.linq
+//systemthreasdding taksa
